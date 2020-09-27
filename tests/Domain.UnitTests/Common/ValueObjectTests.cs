bbcdc911b500f6c762cbeb66eae8c15c4b04f388
Ben Craig <ben.craig@hse.ie>
2020-09-27 17:13:36 +0100

    Create a simple value object to show usage.

---
+++ tests/Domain.UnitTests/Common/ValueObjectTests.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Common;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Domain.UnitTests.Common
+{
+    public class ValueObjectTests
+    {
+        [Fact]
+        public void ShouldDoSomething_WhenSomething()
+        {
+        }
+
+        private class Address : ValueObject
+        {
+            public string Eircode { get; private set; }
+
+            public string Street { get; private set; }
+
+            public string County { get; private set; }
+
+            private Address()
+            {
+            }
+
+            public static Address Create(string eircode, string street, string county)
+            {
+                return new Address
+                {
+                    Eircode = eircode,
+                    Street = street,
+                    County = county
+                };
+            }
+
+            protected override IEnumerable<object> GetAtomicValues()
+            {
+                yield return Eircode;
+                yield return Street;
+                yield return County;
+            }
+        }
+    }
+}
