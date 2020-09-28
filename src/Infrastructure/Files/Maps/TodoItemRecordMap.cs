475f58d109802e0cc269c35f0c0f6b216b999368
Ben Craig <ben.craig@hse.ie>
2020-09-28 14:55:40 +0100

    Add file handling to infrastructure layer

---
+++ src/Infrastructure/Files/Maps/TodoItemRecordMap.cs
@@ -0,0 +1,15 @@
+﻿using Application.TodoLists.Queries.ExportTodos.Models;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace Infrastructure.Files.Maps
+{
+    public class TodoItemRecordMap : ClassMap<TodoItemRecord>
+    {
+        public TodoItemRecordMap()
+        {
+            AutoMap(CultureInfo.InvariantCulture);
+            Map(m => m.Done).ConvertUsing(c => c.Done ? "Yes" : "No");
+        }
+    }
+}
