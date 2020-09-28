4038d302fb4e399e2d80e575de8bc4ce8bafdc7b
Ben Craig <ben.craig@hse.ie>
2020-09-28 12:30:47 +0100

    Start adding handlers for application commands and queries

---
+++ src/Application/TodoItems/Commands/CreateTodoItem/CreateTodoItemCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.TodoItems.Commands.CreateTodoItem
+{
+    public class CreateTodoItemCommand : IRequest<int>
+    {
+        public int ListId { get; set; }
+
+        public string Title { get; set; }
+    }
+}
