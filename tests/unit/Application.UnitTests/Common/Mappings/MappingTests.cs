f9f7f7e51a6b7aa907e48e34d34f47c27837bb61
Ben Craig <ben.craig@hse.ie>
2020-09-28 14:26:35 +0100

    Add mapping tests

---
+++ tests/unit/Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,41 @@
+﻿using Application.Common.Mappings;
+using Application.TodoLists.Queries.GetTodos.Models;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using Xunit;
+
+namespace Application.UnitTests.Common.Mappings
+{
+    public class MappingTests
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
+
+        [Theory]
+        [InlineData(typeof(TodoList), typeof(TodoListDto))]
+        [InlineData(typeof(TodoItem), typeof(TodoItemDto))]
+        public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+        {
+            var instance = Activator.CreateInstance(source);
+
+            _mapper.Map(instance, source, destination);
+        }
+    }
+}
