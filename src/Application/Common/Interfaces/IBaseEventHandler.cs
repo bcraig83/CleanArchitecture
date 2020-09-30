662fe565344beb62cb8ac6d4dd955cbe6eec9453
Ben Craig <ben.craig@hse.ie>
2020-09-30 09:36:04 +0100

    Start figuring out where event handling code should go. Kinda tricky. Might be a trial-and-error approach.

---
+++ src/Application/Common/Interfaces/IBaseEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Common;
+using MediatR;
+
+namespace Application.Common.Interfaces
+{
+    public interface IBaseEventHandler<T> : INotificationHandler<T> where T : BaseEvent
+    {
+    }
+}
