5934453e6ef0e98acdda27c97da038ecaa739d42
Ben Craig <ben.craig@hse.ie>
2020-09-30 09:40:09 +0100

    Put first skeleton event handler in place.

---
+++ src/Application/TodoItems/Handlers/ToDoItemCompletedEventEmailHandler.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common.Interfaces;
+using Domain.Events;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.TodoItems.Handlers
+{
+    public class ToDoItemCompletedEventEmailHandler : IBaseEventHandler<ToDoItemCompletedEvent>
+    {
+        public async Task Handle(
+            ToDoItemCompletedEvent notification,
+            CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
