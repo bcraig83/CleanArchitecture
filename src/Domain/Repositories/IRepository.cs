6f6d25bbff25d0b79bbbbf5dcade321c11fabfc6
Ben Craig <ben.craig@hse.ie>
2020-10-01 12:40:22 +0100

    Start adding some interfaces to implement this...

---
+++ src/Domain/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        // TODO: fill out this interface as we go along.
+
+        IQueryable<TEntity> GetAll();
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+    }
+}
