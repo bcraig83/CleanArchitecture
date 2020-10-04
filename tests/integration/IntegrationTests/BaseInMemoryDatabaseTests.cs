a4223aeb2d3102ed9509e9d9c2ff5059e2a65728
Ben Craig <ben.craig@hse.ie>
2020-10-04 17:32:12 +0100

    Add DB code to ensure DB is cleared, as well as a virtual method to seed the database for a specific test case.

--- tests/integration/IntegrationTests/BaseInMemoryDatabaseTests.cs
+++ tests/integration/IntegrationTests/BaseInMemoryDatabaseTests.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace IntegrationTests
 {
@@ -6,40 +8,35 @@ namespace IntegrationTests
     {
         protected IMediator Mediator;
 
+        // TODO: not sure if this needs to be a member variable? We'll see...
+        protected ServiceProvider ServiceProvider;
+
         protected BaseInMemoryDatabaseTests()
         {
-            Mediator = (IMediator)DependencyInjection.Instance.ServiceProvider.GetService(typeof(IMediator));
+            ServiceProvider = DependencyInjection.Instance.ServiceProvider;
+
+            Mediator = (IMediator)ServiceProvider.GetService(typeof(IMediator));
+
+            Seed();
         }
 
-        //protected DbContextOptions<ApplicationDbContext> ContextOptions { get; }
-        //protected ServiceProvider ServiceProvider;
+        private void Seed()
+        {
+            using var context = (ApplicationDbContext)ServiceProvider.GetService(typeof(ApplicationDbContext));
 
-        //protected BaseInMemoryDatabaseTests(
-        //    DbContextOptions<ApplicationDbContext> contextOptions)
-        //{
-        //    ServiceProvider = DependencyInjection.Instance.BuildServiceProvider();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
 
-        //    ContextOptions = contextOptions;
+            // TODO: First, this is where you might seed standard reference data in the database.
+            // <standard_code_here>
 
-        //    Seed();
-        //}
+            SeedFeatureSpecificData(context);
+        }
 
-        //private void Seed()
-        //{
-        //    // TODO: this is where you might seed standard reference data in the database.
-        //    using (var context = new ApplicationDbContext(ContextOptions))
-        //    {
-        //        context.Database.EnsureDeleted();
-        //        context.Database.EnsureCreated();
-
-        //        SeedFeatureSpecificData(context);
-        //    }
-        //}
-
-        //// If a feature requires it's own specific seed data in the database,
-        //// override this virtual method and perform the seeding there.
-        //protected virtual void SeedFeatureSpecificData(ApplicationDbContext context)
-        //{
-        //}
+        // If a feature requires it's own specific seed data in the database,
+        // override this virtual method and perform the seeding there.
+        protected virtual void SeedFeatureSpecificData(ApplicationDbContext context)
+        {
+        }
     }
 }
