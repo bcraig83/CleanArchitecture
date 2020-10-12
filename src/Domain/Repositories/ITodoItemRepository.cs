563d862a25263c462209544a6b618ea17a03eeda
Ben Craig <ben.craig@hse.ie>
2020-10-12 10:12:45 +0100

    Start cleaning up TODOs

--- src/Domain/Repositories/ITodoItemRepository.cs
+++ src/Domain/Repositories/ITodoItemRepository.cs
@@ -5,7 +5,6 @@ namespace Domain.Repositories
 {
     public interface ITodoItemRepository : IRepository<TodoItem>
     {
-        // TODO: do we need to include "ById" in this method name?
         Task<TodoItem> FindByIdAsync(int id);
     }
 }
--- src/Infrastructure/Persistence/IApplicationDbContext.cs
+++ src/Infrastructure/Persistence/IApplicationDbContext.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Persistence
 {
-    // TODO: I really don't like how EFCore is embedded into the application layer! Would like to abstract this away eventually...
     public interface IApplicationDbContext
     {
         DbSet<TodoList> TodoLists { get; set; }
