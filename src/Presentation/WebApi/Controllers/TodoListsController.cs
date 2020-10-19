67610b37004c51209a62297ef167ac97deae3776
Ben Craig <ben.craig@hse.ie>
2020-10-19 21:50:38 +0100

    Fill out the various REST attributes and put better error handling in place.

--- src/Presentation/WebApi/Controllers/TodoListsController.cs
+++ src/Presentation/WebApi/Controllers/TodoListsController.cs
@@ -1,8 +1,10 @@
 ﻿using Application.TodoLists.Commands.CreateTodoList;
 using Application.TodoLists.Queries.GetTodos;
 using Application.TodoLists.Queries.GetTodos.Models;
+using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 using System.Threading.Tasks;
 
 namespace WebApi.Controllers
@@ -17,10 +19,25 @@ namespace WebApi.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<int>> Create(
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create(
              [FromBody] CreateTodoListCommand command)
         {
-            return await Mediator.Send(command);
+            int result;
+            
+            try
+            {
+                result = await Mediator.Send(command);
+            }
+            catch (ValidationException ve)
+            {
+                return BadRequest(ve.Message);
+            }
+
+            return Ok(result);
         }
     }
 }
