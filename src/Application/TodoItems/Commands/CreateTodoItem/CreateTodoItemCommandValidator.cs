4c02c150c791322dd314c747d0c6587a732da658
Ben Craig <ben.craig@hse.ie>
2020-10-20 16:51:58 +0100

    Improve some of the validation around the incoming commands.

--- src/Application/TodoItems/Commands/CreateTodoItem/CreateTodoItemCommandValidator.cs
+++ src/Application/TodoItems/Commands/CreateTodoItem/CreateTodoItemCommandValidator.cs
@@ -7,8 +7,8 @@ namespace Application.TodoItems.Commands.CreateTodoItem
         public CreateTodoItemCommandValidator()
         {
             RuleFor(v => v.Title)
-                .MaximumLength(200)
-                .NotEmpty();
+                .MaximumLength(200).WithMessage("Max length of title is 200 characters")
+                .NotEmpty().WithMessage("Title field cannot be empty");
         }
     }
 }
