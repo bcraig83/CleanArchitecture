c0b25d0c9e032cacb99483e19ecc72a08f82e093
Ben Craig <ben.craig@hse.ie>
2020-10-29 17:35:43 +0000

    Switch from XBehave to Xunit. No need for the extra baggage here.

--- tests/integration/Application.IntegrationTests/Features/TodoListFeatures.cs
+++ tests/integration/Application.IntegrationTests/Features/TodoListFeatures.cs
@@ -1,10 +1,7 @@
 ﻿using Application.TodoLists.Commands.CreateTodoList;
-using Application.TodoLists.Commands.DeleteTodoList;
 using Domain.Entities;
 using FluentValidation;
 using Shouldly;
-using System;
-using Xbehave;
 using Xunit;
 
 namespace Application.IntegrationTests.Features
@@ -19,124 +16,67 @@ namespace Application.IntegrationTests.Features
             _fixture = fixture;
         }
 
-        [Scenario]
-        public void ShouldThrowException_WhenMinimumFiledsAreNotFilledIn()
+        [Fact]
+        public async void ShouldThrowException_WhenMinimumFiledsAreNotFilledInAsync()
         {
-            Exception exception = null;
-            CreateTodoListCommand command = null;
+            // Arrange
+            var command = new CreateTodoListCommand();
 
-            "Given an empty command"
-                .x(() =>
-                {
-                    command = new CreateTodoListCommand();
-                });
+            // Act
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                await _fixture.SendAsync(command);
+            });
 
-            "When that command is sent"
-                .x(async () =>
-                {
-                    exception = await Record.ExceptionAsync(async () =>
-                    {
-                        await _fixture.SendAsync(command);
-                    });
-                });
-
-            "Then the appropriate expection is thrown"
-                .x(() =>
-                {
-                    exception.ShouldBeOfType<ValidationException>();
-                    exception.Message.ShouldContain("Title: Title is required.");
-                });
+            // Assert
+            exception.ShouldBeOfType<ValidationException>();
+            exception.Message.ShouldContain("Title: Title is required.");
         }
 
-        [Scenario]
-        public void ShouldThrowException_WhenCreatingListWithNonUniqueTitle()
+        [Fact]
+        public async void ShouldThrowException_WhenCreatingListWithNonUniqueTitle()
         {
-            int createdListId = 0;
-            Exception exception = null;
+            // Arrange
+            var createdListId = await _fixture.SendAsync(new CreateTodoListCommand
+            {
+                Title = "Shopping"
+            });
 
-            "Given a Todo list already exists called 'Shopping'"
-                .x(async () =>
+            var createdEntity = await _fixture.FindAsync<TodoList>(createdListId);
+            createdEntity.ShouldNotBeNull();
+
+            // Act
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                var result = await _fixture.SendAsync(new CreateTodoListCommand
                 {
-                    createdListId = await _fixture.SendAsync(new CreateTodoListCommand
-                    {
-                        Title = "Shopping"
-                    });
-
-                    var createdEntity = await _fixture.FindAsync<TodoList>(createdListId);
-                    createdEntity.ShouldNotBeNull();
+                    Title = "Shopping"
                 });
+            });
 
-            "When a new Todo list is injected with the same title"
-                .x(async () =>
-                {
-                    exception = await Record.ExceptionAsync(async () =>
-                    {
-                        var result = await _fixture.SendAsync(new CreateTodoListCommand
-                        {
-                            Title = "Shopping"
-                        });
-                    });
-                });
-
-            "Then the appropriate exception should be thrown"
-                .x(() =>
-                {
-                    exception.ShouldBeOfType<ValidationException>();
-                    exception.Message.ShouldContain("Title: The specified title already exists.");
-                });
-
-            "And we should tidyup after ourselves"
-                .x(async () =>
-                {
-                    await _fixture.SendAsync(new DeleteTodoListCommand
-                    {
-                        Id = createdListId
-                    });
-                });
+            // Assert
+            exception.ShouldBeOfType<ValidationException>();
+            exception.Message.ShouldContain("Title: The specified title already exists.");
         }
 
-        [Scenario]
-        public void ShouldCreateTodoList_WhenValidListIsReceived()
+        [Fact]
+        public async void ShouldCreateTodoList_WhenValidListIsReceived()
         {
-            string userId = null;
-            CreateTodoListCommand command = null;
-            int createdTodoListId = 0;
-            TodoList createdList = null;
+            // Arrange
+            var userId = await _fixture.RunAsDefaultUserAsync();
+            var command = new CreateTodoListCommand()
+            {
+                Title = "New List"
+            };
 
-            "Given that we are running as the default user"
-                .x(async () =>
-                {
-                    userId = await _fixture.RunAsDefaultUserAsync();
-                });
+            // Act
+            var createdTodoListId = await _fixture.SendAsync(command);
 
-            "And given a valid command to create a new TodoList"
-                .x(() =>
-                {
-                    command = new CreateTodoListCommand()
-                    {
-                        Title = "New List"
-                    };
-                });
-
-            "When that command is injected"
-                .x(async () =>
-                {
-                    createdTodoListId = await _fixture.SendAsync(command);
-                });
-
-            "Then we should be able to retrieve the newly created list"
-                .x(async () =>
-                {
-                    createdList = await _fixture.FindAsync<TodoList>(createdTodoListId);
-                });
-
-            "And the list should be correct"
-                .x(() =>
-                {
-                    createdList.ShouldNotBeNull();
-                    createdList.Title.ShouldBe(command.Title);
-                    createdList.CreatedBy.ShouldBe(userId);
-                });
+            // Assert
+            var createdList = await _fixture.FindAsync<TodoList>(createdTodoListId);
+            createdList.ShouldNotBeNull();
+            createdList.Title.ShouldBe(command.Title);
+            createdList.CreatedBy.ShouldBe(userId);
         }
     }
 }
