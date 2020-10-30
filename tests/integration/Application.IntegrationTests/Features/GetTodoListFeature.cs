ebcb1aedb48b6bdb0f7c8c18ead821985c6e1e73
Ben Craig <ben.craig@hse.ie>
2020-10-30 16:52:45 +0000

    Add failing test. Not sure why this get is not working properly...

---
+++ tests/integration/Application.IntegrationTests/Features/GetTodoListFeature.cs
@@ -0,0 +1,59 @@
+﻿using Application.TodoLists.Queries.GetTodos;
+using Domain.Entities;
+using Shouldly;
+using System.Linq;
+using Xunit;
+
+namespace Application.IntegrationTests.Features
+{
+    [Collection("Application test collection")]
+    public class GetTodoListFeature
+    {
+        private readonly ApplicationTestFixture _fixture;
+
+        public GetTodoListFeature(ApplicationTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async void ShouldReturnPriorityLevels()
+        {
+            // Arrange
+            var query = new GetTodosQuery();
+
+            // Act
+            var result = await _fixture.SendAsync(query);
+
+            // Assert
+            result.PriorityLevels.ShouldNotBeEmpty();
+        }
+
+        [Fact]
+        public async void ShouldReturnAllListsAndItems()
+        {
+            await _fixture.AddAsync(new TodoList
+            {
+                Title = "Shopping 30/10/2020",
+                Items =
+                    {
+                        new TodoItem { Title = "Apples"},
+                        new TodoItem { Title = "Milk"},
+                        new TodoItem { Title = "Bread"},
+                        new TodoItem { Title = "Toilet paper" },
+                        new TodoItem { Title = "Pasta" },
+                        new TodoItem { Title = "Tissues" },
+                        new TodoItem { Title = "Tuna" }
+                    }
+            });
+
+            var query = new GetTodosQuery();
+
+            var result = await _fixture.SendAsync(query);
+
+            var createdList = result.Lists.Single(x => x.Title == "Shopping 30/10/2020");
+            createdList.ShouldNotBeNull();
+            createdList.Items.Count.ShouldBe(7);
+        }
+    }
+}
