ecd6954233e61a43b4914b190284a27504797e67
Ben Craig <ben.craig@hse.ie>
2020-11-13 15:41:45 +0000

    Start work on creation path.

---
+++ src/Application/Books/Commands/CreateBook/CreateBookCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Application.Books.Commands.CreateBook
+{
+    public class CreateBookCommand : IRequest<int>
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Language { get; set; }
+        public string Publisher { get; set; }
+        public string ISBN10 { get; set; }
+
+        // TODO: add format
+    }
+}
