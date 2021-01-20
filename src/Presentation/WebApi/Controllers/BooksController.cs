06baa9fe9589a7ad546f91e6feef95910e65cc95
Ben Craig <ben.craig@hse.ie>
2021-01-20 10:11:11 +0000

    Add code to retrieve books from repository.

--- src/Presentation/WebApi/Controllers/BooksController.cs
+++ src/Presentation/WebApi/Controllers/BooksController.cs
@@ -1,7 +1,10 @@
 ﻿using Application.Books.Commands.CreateBook;
+using Application.Books.Queries.GetBooks;
+using Application.Books.Queries.GetBooks.Models;
 using Application.Common.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Net.Mime;
 using System.Threading.Tasks;
 
@@ -14,6 +17,14 @@ namespace WebApi.Controllers
         public ActionResult GetHealth()
         {
             return Ok("Books controller is up!");
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IList<BookDto>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IList<BookDto>>> Get()
+        {
+            var result = await Mediator.Send(new GetBooksQuery());
+            return Ok(result);
         }
 
         [HttpPost]
